c10bc8b45e192cb9c0488259f3fda47877e30e76
imranfix <imran2000hosan@gmail.com>
2022-11-08 22:10:51 +0600

    get read data for serviceAll route client side

--- index.js
+++ index.js
@@ -26,7 +26,16 @@ async function run() {
             const cursor = serviceCollection.find(query);
             const services = await cursor.limit(3).toArray();
             res.send(services);
-        })
+        });
+
+        // get Read data for sell all page: 
+        app.get('/serviceAll', async(req, res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
        
     }
     finally{
