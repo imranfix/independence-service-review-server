9893058328bbcc12a13122b3ce2f0ad74b4c7c45
imranfix <imran2000hosan@gmail.com>
2022-11-09 13:32:47 +0600

    delete operation method done

--- index.js
+++ index.js
@@ -45,10 +45,32 @@ async function run() {
             res.send(service);
         });
 
-        // orders data Create with post (operation):
+        // 4. orders data Create with post (operation):
         app.post('/orders', async(req, res) =>{
             const order = req.body;
             const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+        // 5. call orders data from the databse and read data:
+        app.get('/orders', async(req, res) =>{
+            // console.log(req.query);
+            let query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
+        // 6. delete
+        app.delete( '/orders/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
             res.send(result);
         })
 
